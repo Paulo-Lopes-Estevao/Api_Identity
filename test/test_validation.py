150958a14fc3f00409027ded85078ecd60b17fa1
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-05-21 22:31:05 +0100

    test

--- test/test_validation.py
+++ test/test_validation.py
@@ -12,24 +12,18 @@ class ValidatePerson(unittest.TestCase):
      def test_BI(self):
          tes_bi = Bi()
          vald = tes_bi.rules("006153280LA047")
-         
          self.assertTrue(vald)
 
      def test_BI_service(self):
          test_servi = bi_service()
-         ab=test_servi.verification_bi("006153280LA047")
-         for k in ab:
-            pl = str(k["ID_NUMBER"])
-            self.assertTrue(pl)
+         test_servi.verification_bi("006153280LA047")
+         self.assertTrue(test_servi)
 
 
      def test_NIF_service(self):
         test_servi = nif_service()
-        ab=test_servi.verification_nif("006153280LA047")
-        for k in ab:
-            pl = str(k["ID_NUMBER"])
-            print(pl)
-            self.assertTrue(pl)
+        test_servi.verification_nif("006153280LA047")
+        self.assertTrue(test_servi)
 
 
 if __name__ == '__main__': 
