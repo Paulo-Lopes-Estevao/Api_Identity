652fb9b36ee3d52a2c4d4f146490adf971a5b180
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-05-22 00:32:46 +0100

    tratamento de erro

--- domain/validation/rules/valid.py
+++ domain/validation/rules/valid.py
@@ -7,7 +7,10 @@ class Rule(ValidateInterface):
     def rules(self,value):
         pattern = re.compile(r'^\d{9}[A-Z]{2}\d{3}$')
         search_pattern =pattern.search(value)
-        return search_pattern.group()
+        try:
+            return bool(search_pattern.group())
+        except Exception as ex:
+            return "False"
 
     def message(self):
         return "BI inválido"
