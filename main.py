fc7dfbd9a93d824dade46d625f295ce5d67fbf44
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-14 14:32:37 +0200

    Refactored main.py

--- main.py
+++ main.py
@@ -11,42 +11,47 @@ service_nif = nif_service()
 
 def instance_bi(bi):
     verify_validation = valid_bi.rules(bi)
-    if verify_validation is not False:
+    if verify_validation:
         verify_service = service_bi.verification_bi(bi)
         return verify_service
 
 
 def instance_nif(nif):
     verify_validation = valid_bi.rules(nif)
-    if verify_validation is not False:
+    if verify_validation:
         verify_service = service_nif.verification_nif(nif)
         return bool(verify_service)
 
 
-    
-
 @app.get("/")
 def read_root():
     return {
-        "Name": "Paulo Lopes Estevão",
-        "Developer": "Back-End Developer",
-        "Repository": "https://github.com/Paulo-Lopes-Estevao/Api_Identity",
-        "Midia Social":{
-            "Github":"https://github.com/Paulo-Lopes-Estevao",
-            "Facebook":"https://facebook.com/paulodoposter.poster.1",
-            "Linkedin":"https://www.linkedin.com/in/paulo-lopes-estev%C3%A3o-7a70881b4/",
-            "Telegram":"https://t.me/Paulo_LopesEstevao"
-    },
-
-
+        "Validate BI": "https://ka6xhw.deta.dev/bi/{bi}",
+        "Validate NIF": "https://ka6xhw.deta.dev/nif/{nif}",
+        "Details about maintainer": "https://ka6xhw.deta.dev/about/" 
     }
-
 
 
 @app.get("/bi/{bi}")
 def read_bi(bi : str):
     return instance_bi(bi)
 
+
 @app.get("/nif/{nif}")
 def read_nif(nif : str):
     return instance_nif(nif)
+
+
+@app.get("/about/")
+def read_about():
+    return {
+        "Name": "Paulo Lopes Estevão",
+        "Developer": "Back-End Developer",
+        "Repository": "https://github.com/Paulo-Lopes-Estevao/Api_Identity",
+        "Midia Social": {
+            "Github":"https://github.com/Paulo-Lopes-Estevao",
+            "Facebook":"https://facebook.com/paulodoposter.poster.1",
+            "Linkedin":"https://www.linkedin.com/in/paulo-lopes-estev%C3%A3o-7a70881b4/",
+            "Telegram":"https://t.me/Paulo_LopesEstevao"
+        },
+    }
